72bb9ac4f9d0bb40e2535f7376bde96670e84af1
Christian Dreier <flipper.x3r@googlemail.com>
2016-03-26 20:39:14 +0100

    Init

---
+++ bin2img/bin2img.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from PIL import Image
+
+def bin2img(data):
+    pass
+
+def main(args):
+    print(args)
+
+if __name__ == "__main__":
+    main(sys.argv)
