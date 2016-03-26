4e37c1181004b778fe758da1eec83139cbce1f77
Christian Dreier <flipper.x3r@googlemail.com>
2016-03-26 23:55:04 +0100

    First version

--- bin2img/bin2img.py
+++ bin2img/bin2img.py
@@ -2,13 +2,63 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PIL import Image
+import math
+
+# pip install Pillow
+from PIL import (
+    Image,
+    ImageDraw,
+)
+
+
+def determine_size(data):
+    size = int(math.sqrt(len(data)) + 1)
+    return size, size
+
+
+def getcolor(byteval):
+    color = (
+        ((byteval & 0xc0) >> 6) * 64,
+        ((byteval & 0x38) >> 3) * 32,
+        (byteval & 0x07) * 32,
+    )
+    return color
+
 
 def bin2img(data):
-    pass
+    xsize, ysize = size = determine_size(data)
+    img = Image.new("RGB", size)
+    draw = ImageDraw.Draw(img)
+    try:
+        i = 0
+        for y in range(ysize):
+            for x in range(xsize):
+                color = getcolor(data[i])
+                draw.point((x, y), fill=color)
+                i += 1
+    except IndexError:
+        pass
+    return img
+
+
+def parse_cmdargs(args):
+    infile = args[1]
+    outfile = args[2]
+    return infile, outfile
+
 
 def main(args):
-    print(args)
+    infile, outfile = parse_cmdargs(args)
+    with open(infile, "rb") as f:
+        data = f.read()
+    print("File read")
+
+    img = bin2img(data)
+    print("Image generated")
+
+    img.save(outfile, "PNG", compress_level=9)
+    print('Image stored at "{}"'.format(outfile))
+
 
 if __name__ == "__main__":
     main(sys.argv)
